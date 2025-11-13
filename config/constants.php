6970c001efaae1175feec241349019ae961485eb
Mariam Salim <6229528@edu.vaniercollege.qc.ca>
2025-11-13 09:25:55 -0500

    starting to work on file upload

--- config/constants.php
+++ config/constants.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 // Define the path of the application's root directory.
 define('APP_BASE_DIR_PATH', realpath(dirname(__DIR__, 1)));
+define('APP_UPLOAD_BASE_DIR', APP_BASE_DIR_PATH . '/uploads');
 
 // Holds the name of the app's root directory.
 define('APP_ROOT_DIR_NAME', basename(dirname(__FILE__, 2)));
@@ -11,9 +12,7 @@ define('APP_ROOT_DIR_NAME', basename(dirname(__FILE__, 2)));
 // Define the path of the application's views directory.
 define('APP_VIEWS_PATH', realpath(APP_BASE_DIR_PATH . '/app/Views'));
 
-define
 
-//* HTTP response status code.
 const HTTP_OK = 200;
 const HTTP_CREATED = 201;
 const HTTP_NO_CONTENT = 204;
